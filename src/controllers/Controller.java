46dec7c4bd0a918984497dd9d41a85031d456d63
VasGor <kingstar.gvv@gmail.com>
2018-08-23 18:49:41 +0300

    user interface for choose function

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,6 +1,10 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
@@ -12,6 +16,14 @@ public class Controller {
     private Button btnChooseFile;
     @FXML
     private Label lblFilePath;
+
+    @FXML
+    private LineChart<Float,Float> lineChartExpData;
+    @FXML
+    private NumberAxis xAxis;
+    @FXML
+    private NumberAxis yAxis;
+
 
     public void btnChooseFileClicked(){
         System.out.println("Hi");
@@ -26,5 +38,21 @@ public class Controller {
             lblFilePath.setText(file.getPath());
         }
 
+    }
+
+    public void btnLoadDataClicked(){
+        //Defining X axis
+        xAxis.setLabel("dsfxcvsdfxcvbr");
+
+        XYChart.Series series = new XYChart.Series();
+        series.setName("No of schools in an year");
+
+        series.getData().add(new XYChart.Data(1970, 15));
+        series.getData().add(new XYChart.Data(1980, 30));
+        series.getData().add(new XYChart.Data(1990, 60));
+        series.getData().add(new XYChart.Data(2000, 120));
+        series.getData().add(new XYChart.Data(2013, 240));
+        series.getData().add(new XYChart.Data(2014, 300));
+        lineChartExpData.getData().add(series);
     }
 }
